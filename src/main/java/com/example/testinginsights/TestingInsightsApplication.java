e3cb5f40e1cfdec6a7edfaab0befc586b90ac182
barta3 <baertschi.adrian@gmail.com>
2020-05-01 12:45:27 +0800

    Create initial Spring Boot project

---
+++ src/main/java/com/example/testinginsights/TestingInsightsApplication.java
@@ -0,0 +1,13 @@
+package com.example.testinginsights;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestingInsightsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestingInsightsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/testinginsights/TestingInsightsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testinginsights;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestingInsightsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
